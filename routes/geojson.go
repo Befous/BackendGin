0a2b9228f3266412e6e0b3225b597ffcbe408a28
Ibrohim Mubarok <befousmain@gmail.com>
2024-01-08 00:39:46 +0700

    Ganti method

--- routes/geojson.go
+++ routes/geojson.go
@@ -6,8 +6,8 @@ import (
 )
 
 func GeojsonRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("/MembuatGeojsonPoint", controllers.MembuatGeojsonPoint("publickey", "mongoenv", "befous", "geojson"))
-	incomingRoutes.GET("/MembuatGeojsonPolyline", controllers.MembuatGeojsonPolyline("publickey", "mongoenv", "befous", "geojson"))
-	incomingRoutes.GET("/MembuatGeojsonPolygon", controllers.MembuatGeojsonPolygon("publickey", "mongoenv", "befous", "geojson"))
+	incomingRoutes.POST("/MembuatGeojsonPoint", controllers.MembuatGeojsonPoint("publickey", "mongoenv", "befous", "geojson"))
+	incomingRoutes.POST("/MembuatGeojsonPolyline", controllers.MembuatGeojsonPolyline("publickey", "mongoenv", "befous", "geojson"))
+	incomingRoutes.POST("/MembuatGeojsonPolygon", controllers.MembuatGeojsonPolygon("publickey", "mongoenv", "befous", "geojson"))
 	incomingRoutes.GET("/AmbilDataGeojson", controllers.AmbilDataGeojson("mongoenv", "befous", "geojson"))
 }
